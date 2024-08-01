dbb2d9442086a20807176cd26fda7eada4e431ae
AshikSazal <sojolislm@gmail.com>
2024-08-01 23:57:24 +0600

    fixing some issues

--- .cmd
+++ .cmd
@@ -4,3 +4,4 @@ php artisan vendor:publish --provider="BeyondCode\LaravelWebSockets\WebSocketsSe
 PUSHER_APP_ID, PUSHER_APP_KEY, PUSHER_APP_SECRET, PUSHER_HOST, PUSHER_PORT, PUSHER_SCHEME in .env
 npm install -D tailwindcss postcss autoprefixer
 npx tailwindcss init -p
+npm install
